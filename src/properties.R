fb67cd40c98e6fa9319b4e30441f87ac628c52f6
Vincent Labatut <vince.labatut@gmail.com>
2015-06-26 18:33:19 +0200

    updated properties processing

--- src/properties.R
+++ src/properties.R
@@ -4,8 +4,38 @@
 library("igraph")
 
 
+# set up net folders
 data.folder <- "data/"
-filenames <- c(
-		"CKM_Physicians_Innovation/CKM-Physicians-Innovation_multiplex.edges",	# layerID nodeID nodeID weight=1
-
+folders <- c(
+		"CKM_Physicians_Innovation/",
+		"CS_Aarhus/",
+		"EU_Air_Transport/",
+		"Kapferer_Tailor_Shop/",
+		"Krackhardt8-High_Tech/",
+		"Lazega_Law_Firm/",
+		"London_Transport/",
+		"Padgett_Florence_Families/",
+		"Vickers_Chan_7thGraders/",
 )
+k <- length(folders)
+
+# init property table
+prop <- data.frame(
+	Name=folders, 
+	Nodes=rep(0,k),
+	Links=rep(0,k),
+	stringsAsFactors=FALSE
+)
+
+for(folder in folders)
+{	# init file names
+	net.folder <- paste(data.folder, folder, sep="")
+	net.file <- paste(net.folder, "network.graphml", sep="")
+	prop.file <- paste(net.folder, "properties.txt", sep="")
+	
+	# load graph
+	g <- read.graph(file=net.file,format="graphml")
+	prop <- data.frame()
+	
+	
+}
